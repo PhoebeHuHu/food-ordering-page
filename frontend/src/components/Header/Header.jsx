fcefd86ce02fe11a146687444acb864ddf2a31c1
peike-hu <phoebehu233@gmail.com>
2024-06-13 14:34:46 +1000

    component: add header component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-content">
+                <h2>Order your favourite food here</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae delectus, voluptates praesentium laborum perferendis rem dolor suscipit. Error dolor ab natus nesciunt? Ipsum non vel ut sunt tempore qui totam.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
