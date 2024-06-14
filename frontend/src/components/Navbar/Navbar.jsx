74f9d5559d7fdee1ca039f09e9e76014c3851eb5
peike-hu <phoebehu233@gmail.com>
2024-06-14 18:04:47 +1000

    feat: responsible nav bar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -11,10 +11,17 @@ const Navbar = () => {
         { label: 'mobile app', key: 'mobile-app' },
         { label: 'contact us', key: 'contact-us' }
     ]
+
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const toggleMenu = () => {
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true)
+    }
+
     return (
         <div className='navbar'>
+            <img src={assets.menu_icon} alt="" className='menu-icon' onClick={toggleMenu} />
             <img src={assets.logo} alt="" className="logo" />
-            <ul className="navbar-menu">
+            <ul className={`navbar-menu ${mobileMenu ? '' : 'hide-mobile-menu'}`}>
                 {NAV_ITEMS.map((item) => (
                     <NavbarItem key={item.key} label={item.label} isActive={menu === item.key} onClick={() => setMenu(item.key)} />
                 ))}
@@ -26,6 +33,7 @@ const Navbar = () => {
                     <div className="dot"></div>
                 </div>
                 <button>sign in</button>
+
             </div>
         </div>
     )
