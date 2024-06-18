0e030b9b8861304462023dd6829ad6c3a90f8541
peike-hu <phoebehu233@gmail.com>
2024-06-18 12:58:57 +1000

    style: cart page price and promocode style

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -10,6 +10,7 @@ const Cart = () => {
     ]
     return (
         <div className='cart'>
+            {/* cart items title and items */}
             <div className="cart-items">
                 <div className="cart-items-title">
                     <p>Items</p>
@@ -40,6 +41,40 @@ const Cart = () => {
                     }
                 })}
             </div>
+
+            {/* cart items total price, delivery fee and checkout button */}
+            <div className="cart-bottom">
+                <div className="cart-total">
+                    <h2>Cart Totals</h2>
+                    <div>
+                        <div className="cart-total-detals">
+                            <p>Subtotal</p>
+                            <p>{0}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-detals">
+                            <p>Delivery Fee</p>
+                            <p>{2}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-detals">
+                            <b>Total</b>
+                            <b>{0}</b>
+                        </div>
+                    </div>
+                    <button>PROCEED TO CHECKOUT</button>
+                </div>
+                <div className="cart-promocode">
+                    <div>
+                        <p>If you have a promo code, enter it here</p>
+                        <div className='cart-promocode-input'>
+                            <input type="text" placeholder='promo code' />
+                            <button>Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
